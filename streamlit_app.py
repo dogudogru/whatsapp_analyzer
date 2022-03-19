312784b35138042ba85df1267de3ac316cb63cc7
Doğukan Doğru <dogukandogru1@gmail.com>
2022-03-19 15:24:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,11 +53,11 @@ col1, col2, col3, col4, col5,col6,col7,col8,col9,col10,col11,col12 = st.columns(
 
 with col12:
     increment = st.button('Beğen 👍')
-    st.session_state.count = 0    
+    # st.session_state.count = 0    
     if increment:
-        st.session_state.count += 1
-        metrics = open("metrics.txt", "w")
-        metrics.write(str(st.session_state.count))
+         st.write("Teşekkürler :)")
+    #     metrics = open("metrics.txt", "w")
+    #     metrics.write(str(st.session_state.count))
 
 
 parsedData = []
